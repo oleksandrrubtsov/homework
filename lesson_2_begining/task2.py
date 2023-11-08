3eaa494725b9b55455c26aa9690c4fa6e0346da4
oleksandrubtsov <alexander.rubtsov14@gmail.com>
2023-11-08 13:07:29 +0200

    the very first homework

---
+++ lesson_2_begining/task2.py
@@ -0,0 +1,10 @@
+# Hello World
+print("Hello World")
+
+#print with sep 
+print(1,3,5,7,11, sep='/')
+
+#print with end
+print('First Line', end=' ')
+print('Second Line', end='\n\n\n')
+print('Third Line', end='!')
